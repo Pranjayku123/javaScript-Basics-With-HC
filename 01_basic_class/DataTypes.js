8c4d5db9e2386e114360956287ac10d15b635da3
PRANJAY KUMAR <109287745+Pranjayku123@users.noreply.github.com>
2024-02-19 17:40:48 +0000

    Data Type of javaScript

---
+++ 01_basic_class/DataTypes.js
@@ -0,0 +1,28 @@
+"use strict"  // treat as a newer version of javaScript  ##-##
+
+// All data type of javaScript
+
+
+// # number (2 to power 53)
+// # bigint
+// # string = ""
+// # boolen = true/false
+// # null (standalone value)
+// # undefined 
+// # symbol (use for unique indentification)
+
+
+let prkAge = 24;
+
+let prkName = "Pranjay";
+
+let prkEmail = 'prk@gmail.com';
+
+let prkIsHonest = ture;
+
+let prkCity;
+
+let prkId = " ";
+
+console.log(typeof prkAge);
+
