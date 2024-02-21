a534391e55ebd1939f5b41079386c0dde10f6404
CodeByPrk <109287745+Pranjayku123@users.noreply.github.com>
2024-02-21 17:25:33 +0000

    conversion of datatypes

---
+++ 01_basic_class/conversionOfDatatype.js
@@ -0,0 +1,31 @@
+let gameScore = "90";
+
+//console.log(typeof(gameScore));
+
+//console.log(typeof gameScore);
+
+let discGameScore = Number(gameScore);
+
+//console.log(typeof discGameScore);
+
+// "90" => 33
+
+// "90prk" => NaN
+
+// true = 1 And false = 0
+
+
+
+let prkLogedIn = "";
+
+let prkbooleanlogedin = Boolean(prkLogedIn);
+
+console.log(prkbooleanlogedin);
+
+let num1 = 85;
+
+let stringNumber = String(num1)
+
+console.log(stringNumber);
+
+console.log(typeof(stringNumber));
