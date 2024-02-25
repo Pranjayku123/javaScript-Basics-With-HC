56d4f10610c74aac9dafdd37429905b44a2de9fd
CodeByPrk <109287745+Pranjayku123@users.noreply.github.com>
2024-02-25 12:41:08 +0000

    dataType

---
+++ 01_basic_class/datatype.js
@@ -0,0 +1,55 @@
+
+// data type practice exapmle doc 
+
+//  primitive data type And non-primitived data type or refrence type
+
+// 1.) Primitive data type :
+
+
+// string 
+// number
+// boolean 
+// BigInt
+// null 
+// undefined
+// symbol
+
+
+const score = 100;
+
+const scoreValue = 100.3;
+
+const isLoggedIn = true;
+
+const temp = null;
+
+let userEmail;
+
+console.log(userEmail);
+console.log(temp);
+
+let id = symbol("123");
+
+let anotherId = symbol("123");
+
+// console.log(anotherId);
+// console.log(id);
+
+// 2.) non-primitive or refrence type:-
+
+// Array
+// object
+// function
+
+ 
+
+const myArr = ["prk", "snk","dmk"];
+
+const myOnj = {
+    name = "prkCoder",
+    age = 24
+}
+
+const function () {
+    console.log(typeof Pranjay);
+}
