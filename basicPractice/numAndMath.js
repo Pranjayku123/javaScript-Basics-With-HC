e0fab46f5503fd1c52a1c7a650edda5e3defdffa
CodeByPrk <109287745+Pranjayku123@users.noreply.github.com>
2024-02-28 18:16:24 +0000

    Math And Numbers methods

---
+++ basicPractice/numAndMath.js
@@ -0,0 +1,39 @@
+// number and math related study
+
+const balence = new Number(100);
+
+// console.log(balence);
+// console.log(balence.toString().length());
+console.log(balence.toFixed(3));
+
+const otherNumaber = 12.890;
+ // console.log(otherNumaber.toPrecision(4));
+
+ const numbers = 100000000;
+
+ // console.log(numbers.toLocaleString('en-IN'));
+
+
+ // ==============================Maths=================================
+
+ console.log(Math);
+
+//  console.log(Math.abs(3));
+//  console.log(Math.round(3.9));
+//  console.log(Math.ceil(7.4));
+//  console.log(Math.floor(3.9));
+//  console.log(Math.min(3,4,6,9));
+//  console.log(Math.max(43,4,6,9));
+
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const minVal = 20;
+const maxVal = 50;
+
+console.log(Math.floor(Math.random()*(maxVal-minVal +1))-minVal);
+
+
