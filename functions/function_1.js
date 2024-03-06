1b5e560057430ac729f84ce5497ccf4b1b506a4e
CodeByPrk <109287745+Pranjayku123@users.noreply.github.com>
2024-03-06 17:36:44 +0000

    status:- functions in js

---
+++ functions/function_1.js
@@ -0,0 +1,95 @@
+// function in js
+
+function greetMyName(){
+    console.log("P");
+    console.log("r");
+    console.log("a");
+    console.log("n");
+    console.log("j");
+    console.log("a");
+    console.log("y");
+}
+greetMyName();
+
+function addTwoNumbers(num1, num2){
+    console.log(num1+num2);
+}
+addTwoNumbers(20,40);
+
+function add(num1,num2){
+    const result = num1+num2
+    return result;
+}
+const adds = add(10,10);
+console.log(result);
+
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1, number2){
+
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers(number1, number2){
+
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+
+const result = addTwoNumbers(3, 5)
+
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600] 
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
